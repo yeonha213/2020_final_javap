1927a929d9bf636abf5395b642e088f270e8d2d0
yeonha213 <yeonha213@users.noreply.github.com>
2020-06-17 22:01:27 +0900

    Layout1

--- src/MKCtl.java
+++ src/MKCtl.java
@@ -1,9 +1,28 @@
+import java.awt.*;
 
+class MKLayout extends Frame{
+	
+	Button btn[]= new Button[100];
+	FlowLayout f= new FlowLayout();
+	
+	public MKLayout(String args) {
+		super(args);
+		Panel p = new Panel();
+		setLayout(new GridLayout(9,9));
+		
+		for(int i=1;i<=81;i++) {
+			btn[i] = new Button(i+"");
+			add(btn[i]);
+		}
+		setSize(400,400);
+		setVisible(true);
+		
+	}
+}
 public class MKCtl {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new MKLayout("MineKeeper");
 	}
 
 }
